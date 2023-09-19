6fcdfe0e798391e508d9a5b97bd84a030e2982a0
GoAndDo101 <144616455+GoAndDo101@users.noreply.github.com>
2023-09-19 16:12:42 -0600

    Created magic number prep and core requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // This takes input from the user to choose the number that they're going to guess.
+        Console.WriteLine("What is your Magic Number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // This sets the current guess equal to zero and tests to see if the current guess
+        // equals the magic number.
+        int currentGuess = 0;
+        while (currentGuess != magicNumber){
+            // This asks the user for their guess.
+            Console.WriteLine("Guess a number! ");
+            currentGuess = int.Parse(Console.ReadLine());
+
+            if (currentGuess > magicNumber){
+                Console.WriteLine("TOO HIGH");
+
+            } else if (currentGuess < magicNumber){
+                Console.WriteLine("TOO LOW");
+            } else{
+                Console.WriteLine("YA GOT IT YEAAAH");
+            }
+
+        }
     }
 }
