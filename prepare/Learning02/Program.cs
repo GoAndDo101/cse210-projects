4fef11d3ad1133d1c67e176a115be9133d5ae76c
GoAndDo101 <144616455+GoAndDo101@users.noreply.github.com>
2023-09-26 16:33:07 -0600

    Competed the abstraction activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Hello World!");
+
+        Job job1 = new Job();
+        job1.JobTitle = "software engineer";
+        job1.Company = "Apple";
+        job1.StartYear = 1524;
+        job1.EndYear = 2192;
+        
+        Job job2 = new Job();
+        job2.JobTitle = "Pear Eater";
+        job2.Company = "Microsoft";
+        job2.StartYear = 1000;
+        job2.EndYear = 2023;
+
+
+
+
+        Resume MyResume = new Resume{
+            Name = "Owen Chamberlain"
+        };
+        MyResume.Jobs.Add(job1);
+        MyResume.Jobs.Add(job2);
+
+        MyResume.Display();
+
+
+
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,13 @@
+public class Resume{
+
+    public string Name = "";
+    public List<Job> Jobs = new List<Job>();
+
+    public void Display(){
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine("Jobs: ");
+        foreach (Job j in Jobs){
+            j.Display();
+        }
+    }
+}
