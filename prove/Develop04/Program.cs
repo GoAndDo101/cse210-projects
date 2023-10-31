ade24b25015706378c1078a59b44521088614fd6
GoAndDo101 <144616455+GoAndDo101@users.noreply.github.com>
2023-10-31 13:06:55 -0600

    switch develop and learning

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,17 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment assignment = new Assignment("Ollie", "Surrealist Canadian Sports Reviews" );
-        Console.WriteLine(assignment.GetSummary());
-
-        MathAssignment mathAssignment = new MathAssignment("ollie", "French Fries", "4.5", "11-14");
-        Console.WriteLine(mathAssignment.GetSummary());
-        Console.WriteLine(mathAssignment.GetHomeworkList());
-
-        WritingAssignment writingAssignment = new WritingAssignment("Ollie", "Ancient Proverbs", "How Sumerians Roasted Eachother");
-        Console.WriteLine(writingAssignment.GetSummary());
-        Console.WriteLine(writingAssignment.GetWritingInformation());
-
-
+        Console.WriteLine("Hello Learning04 World!");
     }
 }
