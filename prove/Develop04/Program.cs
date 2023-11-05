cd5283bb68ee251affc954080d3bc1d1dadc3554
GoAndDo101 <144616455+GoAndDo101@users.noreply.github.com>
2023-11-05 01:30:06 -0600

    Wrote up a menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,10 +5,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        BreathingActivity ba1 = new BreathingActivity();
-        ListingActivity la1 = new ListingActivity();
-        ReflectionActivity ra1 = new ReflectionActivity();
-        HyperventilationActivity ha1 = new HyperventilationActivity();
+        bool go = true;
+        while(go == true){
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Hyperventilation Activity");
+            Console.WriteLine("3. Reflection Activity");
+            Console.WriteLine("4. Listing Activity");
+            Console.WriteLine("5. Quit");
+            string input = Console.ReadLine();
+
+            if(input == "1"){
+                BreathingActivity ba = new BreathingActivity();
+            } else if (input == "2"){
+                HyperventilationActivity ha = new HyperventilationActivity();
+            } else if (input == "3"){
+                ReflectionActivity ra = new ReflectionActivity();
+            } else if (input == "4"){
+                ListingActivity la = new ListingActivity();
+            } else if (input == "5"){
+                go = false;
+            } else{
+                Console.WriteLine("Oops that's not a correct input!");
+            }
+
+        }
+
 
     }
 }
